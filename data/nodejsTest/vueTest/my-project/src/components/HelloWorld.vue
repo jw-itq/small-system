9a59e79cefa339e3d547d533898ea5955be78140
shiwanming <1633138551@qq.com>
2019-03-20 19:59:32 +0800

    vuetest1

---
+++ data/nodejsTest/vueTest/my-project/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>{{msg}}</h1>
+    <h1>{{massage}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'welcome to your shiwanming',
+      massage: 'I LOVE YOU'
+    }
+  }
+}
+</script>
